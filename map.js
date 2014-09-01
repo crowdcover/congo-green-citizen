0aca6e17005910d144f5240334882d0edd338459
James Conkling <james.lane.conkling@gmail.com>
2014-09-01 19:37:30 -0400

    add geojson points to map, style, and fit bounds

--- map.js
+++ map.js
@@ -1,5 +1,17 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiamFtZXMtbGFuZS1jb25rbGluZyIsImEiOiJ3RHBOc1BZIn0.edCFqVis7qgHPRgdq0WYsA';
 var map = L.mapbox.map('map')
-    .addLayer(L.tileLayer('http://tiles.osm.moabi.org/moabi_base/{z}/{x}/{y}.png'))
-    .setView([-2, 22.83], 6);
+    .addLayer(L.tileLayer('http://tiles.osm.moabi.org/moabi_base/{z}/{x}/{y}.png'));
+    // .setView([-2, 22.83], 6);
 
+var data = L.mapbox.featureLayer()
+        .loadURL('data.geojson')
+        .on('ready', function(){
+            this.eachLayer(function(marker){
+                marker.setIcon(L.mapbox.marker.icon({
+                            'marker-color': '#5DAF01',
+                            'marker-size': 'small'
+                        }));
+            });
+            map.fitBounds(this.getBounds());
+        })
+        .addTo(map);
